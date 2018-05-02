e936a015dc9084e5e7b3d150d50d1f24c6b7a101
Ryan Baumgardner <r.baumgardner93@gmail.com>
2018-05-02 11:15:13 -0400

    modified gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'img',
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
